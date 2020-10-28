7f6eb24b7fb8c5c9b1adf24450a1141f992df70f
Julian Laubstein <contact@julianlaubstein.de>
2020-10-28 14:25:02 +0100

    Adjusted spacing in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ rm -rf /tmp/logs && mkdir -p /tmp/logs
 
 screen -L -Logfile /tmp/logs/celery.log -dmS celeryWorker bash -c "python -m celery -A sdaps_web worker -E"
 screen -S celeryWorker -X colon "logfile flush 0^M"
+
 screen -L -Logfile /tmp/logs/svelte.log -dmS svelteBundler bash -c "yarn && yarn dev"
 screen -S svelteBundler -X colon "logfile flush 0^M"
+
 python manage.py runserver 0.0.0.0:8080
