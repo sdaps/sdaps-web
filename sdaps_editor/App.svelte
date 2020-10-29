bcde89a771480c8fdbda95d3d12e4e3a60a151d7
Julian Laubstein <contact@julianlaubstein.de>
2020-10-29 11:57:13 +0100

    Added basic DnD action

--- sdaps_editor/App.svelte
+++ sdaps_editor/App.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { dndzone } from "svelte-dnd-action";
+  import { flip } from "svelte/animate";
+
   let sections = [];
   let stepCount = 0;
 
@@ -21,6 +24,16 @@
     sections.splice(startIdx, 1);
 
     sections = sections;
+  }
+
+  const flipDurationMs = 50;
+
+  function handleDndConsider(e) {
+    sections = e.detail.items;
+  }
+
+  function handleDndFinalize(e) {
+    sections = e.detail.items;
   }
 </script>
 
@@ -60,14 +73,21 @@
     <div class="addSection">
       <button class="add" on:click={() => addSection(-1)}>Add Section</button>
     </div>
-    {#each sections as section, idx (section.id)}
-      <div class="section">
-        <p>Section {section.id}</p>
-        <button on:click={() => deleteSection(idx)}>Remove</button>
-      </div>
-      <div class="addSection">
-        <button class="add" on:click={() => addSection(idx)}>Add Section</button>
-      </div>
-    {/each}
+    <div
+      use:dndzone={{ items: sections, flipDurationMs }}
+      on:consider={handleDndConsider}
+      on:finalize={handleDndFinalize}>
+      {#each sections as section, idx (section.id)}
+        <div animate:flip={{ duration: flipDurationMs }}>
+          <div class="section">
+            <p>Section {section.id}</p>
+            <button on:click={() => deleteSection(idx)}>Remove</button>
+          </div>
+          <div class="addSection">
+            <button class="add" on:click={() => addSection(idx)}>Add Section</button>
+          </div>
+        </div>
+      {/each}
+    </div>
   </div>
 </main>
