a7d7f1e71e18411943cf631b36e5e890f4e318f3
Julian Laubstein <contact@julianlaubstein.de>
2020-10-29 19:50:34 +0100

    Added ability to save changes

--- sdaps_editor/SaveQuestionaire.svelte
+++ sdaps_editor/SaveQuestionaire.svelte
@@ -9,12 +9,33 @@
   const BASE_TIMEOUT = 1000;
   let timeout = BASE_TIMEOUT;
 
+  function getCookie(name) {
+    if (!document.cookie) {
+      return null;
+    }
+
+    const xsrfCookies = document.cookie
+      .split(";")
+      .map((c) => c.trim())
+      .filter((c) => c.startsWith(name + "="));
+
+    if (xsrfCookies.length === 0) {
+      return null;
+    }
+    return decodeURIComponent(xsrfCookies[0].split("=")[1]);
+  }
+
   function save() {
+    const csrfToken = getCookie("csrftoken");
+
     fetch(questionnairePath, {
       method: "POST",
       body: JSON.stringify(questionnaire),
+      credentials: "include",
       headers: {
+        Accept: "application/json",
         "Content-Type": "application/json",
+        "X-CSRFToken": csrfToken,
       },
     })
       .then((res) => {
