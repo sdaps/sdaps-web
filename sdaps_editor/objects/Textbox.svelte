4237ba38a43062f91e1b99b0660002fcdedf7118
Julian Laubstein <contact@julianlaubstein.de>
2020-10-31 13:25:45 +0100

    Removed textbox

--- sdaps_editor/objects/Textbox.svelte
+++ sdaps_editor/objects/Textbox.svelte
@@ -4,14 +4,6 @@
   export let textbox: TextboxQuestionnaireObject;
 </script>
 
-<style>
-  textarea {
-    border: solid 1px black;
-    width: 100%;
-    resize: none;
-  }
-</style>
-
 <label> Question: <input type="text" bind:value={textbox.question} /></label>
 
 <label>Expand: <input type="checkbox" bind:checked={textbox.expand} /></label>
@@ -26,8 +18,3 @@
     disabled={textbox.expand}
     bind:value={textbox.height} />
 </label>
-
-<textarea
-  name="answer"
-  rows={textbox.height * window.devicePixelRatio}
-  readonly />
